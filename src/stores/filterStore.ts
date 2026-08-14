c5102a25ab6b3314390ad929675a8b94b4c277b0
Johnny <espgoodlife22@gmail.com>
2026-08-14 03:40:35 +0300

    add Zustand Store

---
+++ src/stores/filterStore.ts
@@ -0,0 +1,21 @@
+import { create } from 'zustand'
+
+import { SearchRequestFilter } from '@/shared/api/types/SearchRequest/SearchRequestFilter'
+
+interface FilterStore {
+	filters: SearchRequestFilter | {}
+	setFilters: (filters: SearchRequestFilter) => void
+	resetFilters: () => void
+}
+
+export const useFilterStore = create<FilterStore>()(set => ({
+	filters: [],
+	setFilters: (value: SearchRequestFilter) => set({ filters: value }),
+	resetFilters: () => set({ filters: [] })
+}))
+
+export const selectFilters = (state: FilterStore) => state.filters
+
+export const selectSetFilters = (state: FilterStore) => state.setFilters
+
+export const selectResetFilters = (state: FilterStore) => state.resetFilters
